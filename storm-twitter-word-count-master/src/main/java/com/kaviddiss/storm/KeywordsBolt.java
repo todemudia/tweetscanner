8dc0dde282827796be8958279a03984e7b95cb38
todemudia <todemudia@gmail.com>
2020-03-03 11:47:41 +0000

    First Commit

---
+++ storm-twitter-word-count-master/src/main/java/com/kaviddiss/storm/KeywordsBolt.java
@@ -0,0 +1,46 @@
+package com.kaviddiss.storm;
+
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Bolt filters out a predefined set of words.
+ */
+public class KeywordsBolt extends BaseRichBolt {
+	
+	private static final long serialVersionUID = 6069146554651714100L;
+	
+	private Set<String> keyword_list = new HashSet<String>(Arrays.asList(new String[] {
+            "labour", "tories", "snp", "libdems", "green","conservative",
+    }));
+    private OutputCollector collector;
+
+    @Override
+    public void prepare(Map map, TopologyContext topologyContext, OutputCollector collector) {
+        this.collector = collector;
+    }
+
+    @Override
+    public void execute(Tuple input) {
+        String lang = (String) input.getValueByField("lang");
+        String word = (String) input.getValueByField("word");
+        if (keyword_list.contains(word)) {
+            collector.emit(new Values(lang, word));
+        }
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+        declarer.declare(new Fields("lang", "word"));
+    }
+}
